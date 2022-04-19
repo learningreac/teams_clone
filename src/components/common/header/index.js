4b8c8b7067c1ca3a196bc6390024dfdc573cd98c
learningreac <learningreact2020@gmai.com>
2022-04-18 21:04:00 -0700

    change search box bg color to light gray

--- src/components/common/header/index.js
+++ src/components/common/header/index.js
@@ -1,34 +1,36 @@
 import './header.css';
 import { MdDashboard } from 'react-icons/md';
-import {GoSearch} from 'react-icons/go';
-import {FaUserAstronaut} from 'react-icons/fa';
-import {BsThreeDots} from 'react-icons/bs'
-
+import { GoSearch } from 'react-icons/go';
+import { FaUserAstronaut } from 'react-icons/fa';
+import { BsThreeDots } from 'react-icons/bs'
+import { IconContext } from 'react-icons';
 
 const Header = () => {
     return (
-        <div className='header'>
-            <div className='header-menu'>
-                <MdDashboard />
-            </div>
-            <div className='header-leftFold'>
-                <label className='header-label'>Microsoft Teams </label>
-            </div>
-            <div className='header-rightFold'>
-               <div className='header-search'>
-                    <GoSearch />
-                    <input placeholder='search'/>
-               </div>
-               <div className='header-profile'>
-                    <div className='header-options'>
-                        <BsThreeDots />
+       
+            <div className='header'>
+                <div className='header-menu'>
+                    <MdDashboard />
+                </div>
+                <div className='header-leftFold'>
+                    <label className='header-label'>Microsoft Teams </label>
+                </div>
+                <div className='header-rightFold'>
+                    <div className='header-search'>
+                        <GoSearch />
+                        <input placeholder='search' />
                     </div>
-                    <div className='header-avatar'>
-                        <FaUserAstronaut />
+                    <div className='header-profile'>
+                        <div className='header-options'>
+                            <BsThreeDots />
+                        </div>
+                        <div className='header-avatar'>
+                            <FaUserAstronaut />
+                        </div>
                     </div>
-               </div>
+                </div>
             </div>
-        </div>
+      
     )
 };
 
